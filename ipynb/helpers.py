a478e42c7a9c9644bde97b6cee7cae1d3066997d
mariavechtomova <vechtomova.maria@gmail.com>
2017-05-11 13:06:25 +0200

    added new data source

--- ipynb/helpers.py
+++ ipynb/helpers.py
@@ -1,24 +1,20 @@
 import re
 import numpy as np
 
-def cleanData(strg,stopwords):
+def cleanData(strg):
     line = re.sub("[^a-zA-Z]",' ',strg)
     line = re.sub('  ',' ',line)
-    #line=line.encode('utf-8')
-    line=str.lower(line)
-    querywords = line.split()
-    resultwords  = [word for word in querywords if word not in stopwords]
-    result = ' '.join(resultwords)
+    result=str.lower(line)
     return result
 
 def getSentenceLength(strg):
     return len(strg.split())
 
-def add_placeholders(strg):
+def add_placeholders(strg, limit):
     line = strg.split()
     length = len(line)
-    if length<200:
-        line = line + ['nowordplaceholder']*(200-length)
+    if length<limit:
+        line = line + ['nowordplaceholder']*(limit-length)
     result = ' '.join(line)
     return result
 
